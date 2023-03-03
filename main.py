564a5501c30ace51a27de82f83760ede9d6e21a4
Aman Agarwal <amanagarwal@gmx.us>
2023-03-03 19:22:09 +0000

    SQLite database initialized (upto flask db init)

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-from app import flaskapp
+from app import flaskapp, db
+from app.models import User, Post
+
+
+@flaskapp.shell_context_processor
+def make_shell():
+    return ({'db': db, 'User': User, 'Post': Post})
+
 
 flaskapp.run(host='0.0.0.0', port=81)
